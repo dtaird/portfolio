a385c2a1ec6bb281ef21f9b9630c4f94d53c3bfc
dtaird <dtair@icloud.com>
2025-06-23 20:27:32 +0500

    font fix

--- src/App.js
+++ src/App.js
@@ -48,6 +48,9 @@ export default function App() {
             main: "rgb(162, 110, 182)",
           },
         },
+        typography: {
+          fontFamily: 'Jost, Roboto',
+        },
       }),
     [mode]
   );
